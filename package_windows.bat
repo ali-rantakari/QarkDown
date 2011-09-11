5a2e4a1fe50b69e3ab823375580706856f897b23
Ali Rantakari <Ali Rantakari@alis-winxp-vm.localdomain>
2011-09-11 21:49:34 +0300

    Add packaging script for Windows. Add also 7-zip executable used by the former.

---
+++ package_windows.bat
@@ -0,0 +1,44 @@
+echo off
+echo Remember to build a release binary from Qt Creator first.
+pause
+
+SET packagedir=qarkdown-windows
+SET zipfile=qarkdown-windows.zip
+SET default_qtversion=4.7.3
+
+echo Type in the Qt version to use, or just press enter to use the default (%default_qtversion%)
+SET /P qtversion=[Qt version:]
+IF [%qtversion%]==[] SET qtversion=%default_qtversion%
+
+echo Creating package directory: %packagedir%
+mkdir %packagedir%
+
+call :COPYTOPACKAGE C:\QtSDK\mingw\bin\mingwm10.dll
+call :COPYTOPACKAGE C:\QtSDK\mingw\bin\libgcc_s_dw2-1.dll
+call :COPYTOPACKAGE C:\QtSDK\Desktop\Qt\%qtversion%\mingw\bin\QtCore4.dll
+call :COPYTOPACKAGE C:\QtSDK\Desktop\Qt\%qtversion%\mingw\bin\QtGui4.dll
+call :COPYTOPACKAGE qarkdown-build-desktop\Release\qarkdown.exe
+call :COPYTOPACKAGE README.md
+call :COPYTOPACKAGE LICENSE.md
+
+IF EXIST %zipfile% (
+echo Deleting existing zip file: %zipfile%
+del %zipfile%
+)
+
+echo Zipping up the package to: %zipfile%
+tools\7z.exe a %zipfile% %packagedir%
+
+echo Removing package directory: %packagedir%
+rmdir /S /Q %packagedir%
+
+echo DONE.
+pause
+goto :EOF
+
+
+Rem Function COPYTOPACKAGE
+:COPYTOPACKAGE
+echo Copying: %*
+copy %* %packagedir%\.
+goto :EOF
