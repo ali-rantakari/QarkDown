69d7554c35a0028bdb4b09ff706395636b8605eb
Ali Rantakari <Ali Rantakari@alis-winxp-vm.localdomain>
2011-10-30 21:57:43 +0200

    Update the Windows packaging scripts.
    Get the app version dynamically. Zip up the installer, as well as the readme
    and license files.

--- innosetup.iss
+++ innosetup.iss
@@ -2,21 +2,20 @@
 
 [Setup]
 AppName=QarkDown
-AppVersion=0.3.0
+AppVersion={#appversion}
 DefaultDirName={pf}\QarkDown
 DefaultGroupName=QarkDown
 LicenseFile=LICENSE.md
 
 [Files]
-Source: "qarkdown-windows\qarkdown.exe"; DestDir: "{app}"
-Source: "qarkdown-windows\mingwm10.dll"; DestDir: "{app}"
-Source: "qarkdown-windows\libgcc_s_dw2-1.dll"; DestDir: "{app}"
-Source: "qarkdown-windows\QtCore4.dll"; DestDir: "{app}"
-Source: "qarkdown-windows\QtGui4.dll"; DestDir: "{app}"
+Source: "qarkdown-windows-installerfiles\qarkdown.exe"; DestDir: "{app}"
+Source: "qarkdown-windows-installerfiles\mingwm10.dll"; DestDir: "{app}"
+Source: "qarkdown-windows-installerfiles\libgcc_s_dw2-1.dll"; DestDir: "{app}"
+Source: "qarkdown-windows-installerfiles\QtCore4.dll"; DestDir: "{app}"
+Source: "qarkdown-windows-installerfiles\QtGui4.dll"; DestDir: "{app}"
 Source: "README.md"; DestDir: "{app}"; DestName: "README.txt"; Flags: isreadme
 Source: "LICENSE.md"; DestDir: "{app}"; DestName: "LICENSE.txt";
 
 [Icons]
 Name: "{group}\QarkDown"; Filename: "{app}\qarkdown.exe"; WorkingDir: "{app}"
 Name: "{group}\Uninstall QarkDown"; Filename: "{uninstallexe}"
-
--- package_windows.bat
+++ package_windows.bat
@@ -2,14 +2,21 @@ echo off
 echo Remember to build a release binary from Qt Creator first.
 pause
 
-SET packagedir=qarkdown-windows
+python get_version.py > version-temp.txt
+SET /P appversion= < version-temp.txt
+del version-temp.txt
+
+SET packagedir=qarkdown-windows-installerfiles
 SET default_qtversion=4.8.0
+SET innosetup="C:\Program Files\Inno Setup 5\ISCC.exe"
+SET installerfile=QarkDown-Installer-%appversion%
+SET zipfile=QarkDown-Windows-%appversion%.zip
 
 echo Type in the Qt version to use, or just press enter to use the default (%default_qtversion%)
 SET /P qtversion=[Qt version:]
 IF [%qtversion%]==[] SET qtversion=%default_qtversion%
 
-echo Creating package directory: %packagedir%
+echo ----- Creating package directory: %packagedir%
 mkdir %packagedir%
 
 call :COPYTOPACKAGE C:\QtSDK\mingw\bin\mingwm10.dll
@@ -20,7 +27,24 @@ call :COPYTOPACKAGE qarkdown-build-desktop-Qt_4_8_0_RC_for_Desktop_-_MinGW__Qt_S
 call :COPYTOPACKAGE README.md
 call :COPYTOPACKAGE LICENSE.md
 
-echo DONE. You can now generate the installer package.
+echo ----- Creating installer via Inno Setup
+%innosetup% innosetup.iss /o. /f%installerfile% /dappversion=%appversion%
+
+IF EXIST %zipfile% (
+echo ----- Deleting existing zip file: %zipfile%
+del %zipfile%
+)
+
+echo ----- Zipping up the package to: %zipfile%
+tools\7z.exe a %zipfile% %installerfile%.exe README.md LICENSE.md
+
+echo ----- Removing package directory: %packagedir%
+rmdir /S /Q %packagedir%
+
+echo ----- Removing installer exe
+del %installerfile%.exe
+
+echo DONE.
 pause
 goto :EOF
 
