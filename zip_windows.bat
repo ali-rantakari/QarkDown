3dceacb7ad917e11edc3b2007ece5ec9599aaf78
Ali Rantakari <Ali Rantakari@alis-winxp-vm.localdomain>
2011-10-30 17:43:18 +0200

    Make the windows packaging script simply collect all the installable files.
    Add separate "zipping" windows package script.

--- zip_windows.bat
+++ zip_windows.bat
@@ -3,6 +3,7 @@ echo Remember to build a release binary from Qt Creator first.
 pause
 
 SET packagedir=qarkdown-windows
+SET zipfile=qarkdown-windows.zip
 SET default_qtversion=4.8.0
 
 echo Type in the Qt version to use, or just press enter to use the default (%default_qtversion%)
@@ -20,7 +21,18 @@ call :COPYTOPACKAGE qarkdown-build-desktop-Qt_4_8_0_RC_for_Desktop_-_MinGW__Qt_S
 call :COPYTOPACKAGE README.md
 call :COPYTOPACKAGE LICENSE.md
 
-echo DONE. You can now generate the installer package.
+IF EXIST %zipfile% (
+echo Deleting existing zip file: %zipfile%
+del %zipfile%
+)
+
+echo Zipping up the package to: %zipfile%
+tools\7z.exe a %zipfile% %packagedir%
+
+echo Removing package directory: %packagedir%
+rmdir /S /Q %packagedir%
+
+echo DONE.
 pause
 goto :EOF
 
