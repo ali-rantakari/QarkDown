82361f22b0a5a887ca3c34db2d3086510087a533
Ali Rantakari <Ali Rantakari@localhost>
2011-11-08 23:43:11 +0200

    Bump version to 0.3.1

--- src/qarkdownapplication.cpp
+++ src/qarkdownapplication.cpp
@@ -9,7 +9,7 @@ struct applicationVersion
     int major;
     int minor;
     int tiny;
-} appVersion = {0, 3, 0};
+} appVersion = {0, 3, 1};
 
 #define kCopyrightYearStr "2011"
 #define kWebsiteURL "http://hasseg.org/qarkdown"
